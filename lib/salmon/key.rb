93af8d01f85df4acbc2ef57e2420b7ccc5ea660e
Bob Aman <bobaman@google.com>
2010-12-08 16:10:33 -0800

    Added parsing and generation for .pem and .der keys.

--- lib/salmon/key.rb
+++ lib/salmon/key.rb
@@ -1,4 +1,5 @@
 require 'salmon'
+require 'openssl'
 
 module Salmon
   class Key
@@ -19,12 +20,38 @@ module Salmon
       )
     end
 
+    def to_pem
+      return self.to_openssl.to_pem
+    end
+
+    def to_der
+      return self.to_openssl.to_der
+    end
+
+    def to_openssl
+      @openssl_key ||= (begin
+        key = OpenSSL::PKey::RSA.new
+        key.n = self.modulus
+        key.e = self.exponent
+        key
+      end)
+      return @openssl_key
+    end
+
     def self.parse_magic_key(data)
       modulus, exponent = data.match(MAGIC_KEY_PATTERN)[1..2]
-      Salmon::Key.new(
+      return Salmon::Key.new(
         Salmon.base64url_to_i(modulus),
         Salmon.base64url_to_i(exponent)
       )
+    end
+
+    def self.parse_pem(data)
+      openssl_key = OpenSSL::PKey::RSA.new(data)
+      return Salmon::Key.new(openssl_key.n, openssl_key.e)
+    end
+    class <<self
+      alias_method :parse_der, :parse_pem
     end
   end
 end
--- spec/salmon/key_spec.rb
+++ spec/salmon/key_spec.rb
@@ -43,4 +43,67 @@ describe Salmon::Key do
       'ylBCuspNbt66wvcjfJOhYpRo_StHnlD3toX-D4_uQph8M.AQAB'
     )
   end
+
+  it 'should parse a .pem correctly' do
+    key = Salmon::Key.parse_pem(<<-PEM
+-----BEGIN RSA PUBLIC KEY-----
+MEgCQQCZWBjxE3pREFOy2d2+ZRQ9vhRlN3C/CZ11IoTnP+O8pQQrrKTW7eusL3I3
+yToWKUaP0rR55Q97aF/g+P7kKYfDAgMBAAE=
+-----END RSA PUBLIC KEY-----
+PEM
+    )
+    key.modulus.should == (
+      '803128378907519656502289154656359136834494406215410' +
+      '050964539889229343337085989194330643990745488374753' +
+      '4493461257620351548796452092307094036643522661681091'
+    ).to_i
+    key.exponent.should == '65537'.to_i
+  end
+
+  it 'should generate a .pem correctly' do
+    modulus = (
+      '803128378907519656502289154656359136834494406215410' +
+      '050964539889229343337085989194330643990745488374753' +
+      '4493461257620351548796452092307094036643522661681091'
+    ).to_i
+    exponent = '65537'.to_i
+    key = Salmon::Key.new(modulus, exponent)
+    key.to_pem.should == <<-PEM
+-----BEGIN RSA PUBLIC KEY-----
+MEgCQQCZWBjxE3pREFOy2d2+ZRQ9vhRlN3C/CZ11IoTnP+O8pQQrrKTW7eusL3I3
+yToWKUaP0rR55Q97aF/g+P7kKYfDAgMBAAE=
+-----END RSA PUBLIC KEY-----
+PEM
+  end
+
+  it 'should parse a .der correctly' do
+    key = Salmon::Key.parse_der(
+      "0H\002A\000\231X\030\361\023zQ\020S\262\331\335\276e\024=\276\024e7p" +
+      "\277\t\235u\"\204\347?\343\274\245\004+\254\244\326\355\353\254/r7" +
+      "\311:\026)F\217\322\264y\345\017{h_\340\370\376\344)\207\303\002" +
+      "\003\001\000\001"
+    )
+    key.modulus.should == (
+      '803128378907519656502289154656359136834494406215410' +
+      '050964539889229343337085989194330643990745488374753' +
+      '4493461257620351548796452092307094036643522661681091'
+    ).to_i
+    key.exponent.should == '65537'.to_i
+  end
+
+  it 'should generate a .der correctly' do
+    modulus = (
+      '803128378907519656502289154656359136834494406215410' +
+      '050964539889229343337085989194330643990745488374753' +
+      '4493461257620351548796452092307094036643522661681091'
+    ).to_i
+    exponent = '65537'.to_i
+    key = Salmon::Key.new(modulus, exponent)
+    key.to_der.should == (
+      "0H\002A\000\231X\030\361\023zQ\020S\262\331\335\276e\024=\276\024e7p" +
+      "\277\t\235u\"\204\347?\343\274\245\004+\254\244\326\355\353\254/r7" +
+      "\311:\026)F\217\322\264y\345\017{h_\340\370\376\344)\207\303\002" +
+      "\003\001\000\001"
+    )
+  end
 end
